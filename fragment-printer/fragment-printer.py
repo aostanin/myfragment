9cac9ed1ac79ac4ae2633224f50b7082ac6b0b17
Andrei Ostanin <aostanin@gmail.com>
2014-08-12 21:52:55 +0900

    update printer

--- fragment-printer/fragment-printer.py
+++ fragment-printer/fragment-printer.py
@@ -11,7 +11,7 @@ from PySide import QtCore, QtGui
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--image', nargs=5, required=True, help='Input image (5 total)')
-    parser.add_argument('-u', '--url', required=True, help='URL for the QR code')
+    parser.add_argument('-u', '--url', required=False, help='URL for the QR code')
 
     return parser.parse_args()
 
@@ -32,11 +32,12 @@ def create_qr_code(url):
     return QtGui.QImage(image_data, w, h, QtGui.QImage.Format_ARGB32)
 
 
-def print_card(images, qrcode, url, card_size=(148, 100)):
+def print_card(images, card_size=(148, 100)):
     app = QtGui.QApplication(sys.argv)
 
     printer = QtGui.QPrinter()
 
+    printer.setCopyCount(2)
     printer.setOrientation(QtGui.QPrinter.Landscape)
     printer.setColorMode(QtGui.QPrinter.GrayScale)
     printer.setPageMargins(0, 0, 0, 0, QtGui.QPrinter.Millimeter)
@@ -54,7 +55,11 @@ def print_card(images, qrcode, url, card_size=(148, 100)):
         scaled_image = image.scaled(image_rect.size(), QtCore.Qt.KeepAspectRatio)
         adjusted_rect = QtCore.QRect(image_rect)
         adjusted_rect.setSize(scaled_image.size())
-        adjusted_rect.translate((image_rect.width() - scaled_image.width()) / 2, (image_rect.height() - scaled_image.height()) / 2)
+        if image_rect.top() >= image_rect.height():
+            dy = 0
+        else:
+            dy = image_rect.height() - scaled_image.height()
+        adjusted_rect.translate((image_rect.width() - scaled_image.width()) / 2, dy)
         painter.drawImage(adjusted_rect, image)
 
         image_rect.translate(image_rect.width(), 0)
@@ -62,23 +67,16 @@ def print_card(images, qrcode, url, card_size=(148, 100)):
             image_rect.moveTop(image_rect.height())
             image_rect.moveLeft(0)
 
-    qr_side_margin = image_rect.width() * 0.25
-    qr_top_margin = image_rect.height() * 0.1
-    qr_size = min(image_rect.width(), image_rect.height()) - 2 * qr_side_margin
-    qr_rect = QtCore.QRect(image_rect.x() + qr_side_margin, image_rect.y() + qr_top_margin, qr_size, qr_size)
-    qrcode = qrcode.scaled(qr_rect.size(), QtCore.Qt.KeepAspectRatio)
-    painter.drawImage(qr_rect, qrcode)
-
     painter.end()
 
 
 if __name__ == '__main__':
     args = parse_args()
 
-    qrcode = create_qr_code(args.url)
+    #qrcode = create_qr_code(args.url)
 
     images = [QtGui.QImage(fn) for fn in args.image]
     if not all(not image.isNull() for image in images):
         raise Exception('Failed to load one or more images')
 
-    print_card(images, qrcode, args.url)
+    print_card(images)
